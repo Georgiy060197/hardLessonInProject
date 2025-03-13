70cc66a649d2576a99f8bf6a7b00d57297ed9b9e
Georgiy Truseev <igor.truseev@mail.ru>
2025-03-13 20:08:21 +0300

    Выполнение усложненного задания lesson04

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-const num = 1235
-const str = num.toString()
-let rezult = 1
-for (let i = 0; i < str.length; i++) {
-    rezult = rezult * Number(str[i])
+function stringType(x) {
+    if (typeof x === "string") {
+        console.log(stringControl(x))
+    } else {
+        console.log("It is not string")
+    }
 }
-console.log(rezult)
-const powNum = rezult ** 3
-console.log(powNum)
-const strPowNum = powNum.toString()
-const twoNum = strPowNum.slice(0, 2)
-console.log(twoNum)
+function stringControl(x) {
+    const str = x.trim()
+    if (str.length > 30) {
+        console.log(str.substr(0, 30) + "...")
+    } else {
+        console.log("You string have <= 30 symbol")
+    }
+}
+stringType("Если строка более 30 знаков - то после 30-го символа часть текста скрывается и вместо них появляются три точки")
